001290b9f46a83d1d59b19c1a8bd49a119aa5607
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-07-26 15:09:00 +0500

    completed

--- task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
@@ -10,12 +10,11 @@ public class ChainParserBuilder {
     }
 
     public ChainParserBuilder setParser(AbstractTextParser abstractTextParser) {
-        // Write your code here!
+        parsers.add(abstractTextParser);
         return this;
     }
 
     public AbstractTextParser build() {
-        // Write your code here!
         return null;
     }
 }
