9cfc59082017ce7373a402f819da317da9e30494
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-07-26 14:16:17 +0500

    completed

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,13 +1,22 @@
 package com.mjc.stage2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShopStock {
     private final List<Product> productList;
 
+
     public ShopStock(List<Product> productList) {
         this.productList = productList;
     }
 
-    // Write your code here!
+    public List<Product> executeFilteringStrategy(FilteringStrategy filteringStrategy) {
+        List<Product> res = new ArrayList<>();
+        for (Product product : productList) {
+            if (filteringStrategy.filter(product))
+                res.add(product);
+        }
+        return res;
+    }
 }
